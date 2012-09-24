bb2506c00d2bfd58bc68a6ffa1fa47af1e164e00
Tom Robinson <robinson.tj@gmail.com>
2012-09-24 15:50:50 +1000

    Refactored Code, Generalised for Multi-plot, Automatic + Fancy Legend

--- RRPlotter.py
+++ RRPlotter.py
@@ -1,62 +1,96 @@
 #-------------------------------------------------------------------------------
-# Name:        module2
-# Purpose:
+# Name:        RRPlotter.py
+# Purpose:     Function to fit and plot a RR curve
 #
-# Author:      user19
+# Author:      Tom Robinson
 #
 # Created:     04/09/2012
-# Copyright:   (c) user19 2012
-# Licence:     <your licence>
+# Copyright:   (c) Tom Robinson 2012
 #-------------------------------------------------------------------------------
+
 from scipy.optimize import curve_fit
-from numpy import exp, log10, linspace
+from numpy import *
 from matplotlib import pyplot
 from matplotlib import scale as mscale
 from RRYScale import RRYScale
 from tempfile import mkstemp
 from os import close
 
+
 def RosinRammler(x,k,n):
     return 100*exp(-(x/k)**n)
+
 
 def FitRR(x,y):
     popt, pcov = curve_fit(RosinRammler,x,y)
     k, n = popt[0], popt[1]
     return k, n
 
+
 def Pass2Retain(PP):
     return [PP[0] - x for x in PP]
 
-def PlotRR(x,y):
-    pyplot.clf()
-    k, n = FitRR(x,y)
-    f, fn = mkstemp(suffix='.png')
 
-    rrx = linspace(min(x),max(x),num=50,endpoint=True)
-    rry = RosinRammler(rrx,k,n)
+def AddPoints(x,y,yname):
+    pyplot.hold(True)
+    pyplot.plot(x, y, marker='.', label=str(yname[0]))
 
-    pyplot.plot(x, y, 'ro')
-    pyplot.plot(rrx,rry,'g-')
 
+def AddTrend(rrx,rry,yname):
+    pyplot.hold(True)
+    pyplot.plot(rrx, rry, marker='--', label=str(yname[0]))
+
+
+def PlotSetXRange(x):
+    minex = (log10(min(x))+(-log10(min(x))%1))//1 - 1 # Always round up then take 1
+    maxex = log10(max(x))//1 + 1 #Always round down then add 1
+    pyplot.axis([10**minex,10**maxex,0.1,99.8])
+
+
+def PlotSetScale():
     mscale.register_scale(RRYScale)
     pyplot.grid(b=True,which='major',axis='y')
     pyplot.grid(b=True,which='both',axis='x')
-    pyplot.axis([10**-2,10**2,0.1,99.8])
     pyplot.gca().set_yscale('rry')
     pyplot.gca().set_xscale('log')
     pyplot.gca().invert_yaxis()
 
-    s ='k = %.2f, n = %.2f' % (k, n)
-    pyplot.text(10**-1.7,9,s)
+
+def PlotSetAnotation():
     pyplot.xlabel('Screen Size [mm]')
     pyplot.ylabel('Retained on Screen [%]')
     pyplot.title('Rosin-Rammler Plot')
-    pyplot.savefig(fn,dpi=300,format='png',bbox_inches='tight',pad_inches=0.1)
+    pyplot.legend(loc=0, borderaxespad=0.5,
+        prop={'size':10},fancybox=True, shadow=True, ncol=2)
+
+
+def PlotAddData(x,y,yname,trend):
+    for yi, ynamei in zip(y, yname):
+        AddPoints(x,yi,ynamei)
+        if trend == True:
+            k, n = FitRR(x,yi)
+            rrx = linspace(min(x),max(x),num=50,endpoint=True)
+            rry = RosinRammler(rrx,k,n)
+            rryname = ynamei[0] + ' RR Trend'
+            AddTrend(rrx, rry, rryname)
+
+
+def PlotRR(x, y, yname = [['Size Distribution']], trend = False, passing = True):
+    f, fn = mkstemp(suffix='.png')
+    PlotAddData(x, y, yname, trend)
+    PlotSetXRange(x)
+    PlotSetScale()
+    PlotSetAnotation()
+    #pyplot.show()
+    pyplot.savefig(fn,dpi=300,format='png',
+        bbox_inches='tight',pad_inches=0.1)
     close(f)
-    return k, n, fn
+    pyplot.clf()
+    return fn
+
 
 if __name__ == '__main__':
-    x =  [0.08, 0.50, 1.25, 2, 4, 6.3, 8, 12.5, 16, 40]
-    y = [95.61, 87.71, 82.45, 79.78, 73.28, 66.98, 63.21, 55.4, 49.91, 23.95]
-    k, n, fn = PlotRR(x,y)
-    print k, n, fn
+    x = [[50,11.2,3,1,0.125]]
+    y =[[0,2.6,16.5,31.8,42.9],[0,18.1,35.3,46.6,56.8],[0,28.2,53.9,63.1,74.3]]
+    yname = [['Roto North'],['Roto South #1'],['Roto South #2']]
+    print PlotRR(x[0], y, yname)
